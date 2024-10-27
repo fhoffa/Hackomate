bc557e2feb338b052284ff9513169b8d45e2813e
Felipe Hoffa <fhoffa@users.noreply.github.com>
2024-10-27 14:34:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,16 @@ st.write(
     "Step 1 is collecting people and their LinkedIn resumes, also collecting hackathon sponsors and their description"
 )
 
+db_participants = [
+    ['Felipe Hoffa', 'https://linkedin.com/in/hoffa', 'Data and Python'],
+    ['Asako', 'https://www.linkedin.com/in/asako-hayase-924508ba/', 'Marketing and UX and ...'],
+    ['Aninda', 'https://www.linkedin.com/in/aninda-sengupta/', 'Java and Python and  ...'],
+    ['Gulsher', 'https://www.linkedin.com/in/gulsher-kooner/', 'Data and Spark and ...'],
+]    
+
+
+st.write(db_participants)
+
 
 st.write(
     "Step 2: Once a participan is logged in, they can suggest projects. The LLM will suggest other ideas, and suggest what tools from the sponsors can be used."
