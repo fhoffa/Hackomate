cce200cb9f1da698c0e11864ffebec3a56ea97cf
Felipe Hoffa <fhoffa@users.noreply.github.com>
2024-10-27 14:30:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Hackomate")
+
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Step 1 is collecting people and their LinkedIn resumes, also collecting hackathon sponsors and their description"
+)
+
+
+st.write(
+    "Step 2: Once a participan is logged in, they can suggest projects. The LLM will suggest other ideas, and suggest what tools from the sponsors can be used."
+)
+
+st.write(
+    "Step 3: People can ask to participate in projects, and the project owner can choose who they want in their team based on their skills."
 )
